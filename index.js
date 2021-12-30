8a8c1069bd201f46707fc930c4379cf38fd6b154
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-29 19:59:20 -0700

    commit tree bd5d40cae41b8a5a7c87fad9249f67a15b5c95ca

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let length = word.length
+  for (i=0;i<word.length;i++){
+      if(word.charAt(i) != word.charAt(length-1-i)){
+        return false
+      }else{
+        return true
+      }}
+
 }
 
 /* 
   Add your pseudocode here
+  check array for a string
+  check to see if the string is greater than one
+  check to see if the first and last letter are equal to each other
+  return true
+  all else return false
 */
 
 /*
   Add written explanation of your solution here
+  if the string is all alone the string will be the same forward and backwards. if the strings first and
+  last letter match then there is a high probability of it matching.
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +30,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log('=>', isPalindrome('tattarrattat'))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
